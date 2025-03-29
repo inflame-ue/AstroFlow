ffe2ea4e18260ea9cf50e7046a233fddb80edc85
Ilya Oberemok <i.oberemok777@gmail.com>
2025-03-29 17:52:03 +0200

    refactor file structure after merge

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from flask import Flask, render_template
-from flask import request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        return redirect(url_for('simulation.index')) 
     return render_template('form.html')
 
 if __name__ == '__main__':
