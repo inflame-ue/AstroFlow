7750e4c06e8b1bdbef26a7a684171af6a10beba3
Ilya Oberemok <i.oberemok777@gmail.com>
2025-03-29 17:43:08 +0200

    add basic post request logic

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
+from flask import request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        return redirect(url_for('simulation.index')) 
     return render_template('form.html')
 
 if __name__ == '__main__':
