959e55f5fa312efafcce6dd3e3e3329afe891c9f
Ilya Oberemok <i.oberemok777@gmail.com>
2025-03-30 07:12:15 +0300

    add error handling endpoints

--- main.py
+++ main.py
@@ -30,11 +30,32 @@ def index():
 def simulation():
     status = request.args.get('status')
     message = request.args.get('message')
+    form_data = session.get('form_data')
+
+    if not form_data:
+        return redirect(url_for('bad_request'))
 
     return render_template('simulation.html', 
                            status=status, 
                            message=message,
-                           form_data=session.get('form_data')) # pass form data to simulation page
+                           form_data=form_data) # pass form data to simulation page
+
+
+# Error Handling Endpoints
+@app.errorhandler(400)
+def bad_request(e):
+    return render_template('400.html'), 400
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+# API endpoints
 
 @app.route('/api/form_data')
 def get_form_data():
